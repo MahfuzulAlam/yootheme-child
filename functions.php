a50074105ac559701a19d052907fbbe6ac0160ba
Mahfuz <a_mahfuzul@yahoo.com>
2022-01-12 15:11:34 +0600

    add shortcode single_listing_download

--- functions.php
+++ functions.php
@@ -19,4 +19,15 @@ add_filter('directorist_custom_field_meta_key_field_args', function ($args) {
     return $args;
 });
 
-?>
+add_shortcode( 'single_listing_download', 'single_listing_download_function' );
+function single_listing_download_function() {
+    if(is_user_logged_in()){
+        $downloadable_file_link = get_post_meta(get_the_ID(), '_download-url', true);
+        if(!empty($downloadable_file_link))
+            return '<div class="pl-contact">To download this music for FREE to use in your vidoes:<div class="pl-cta"><a href="'.esc_url($downloadable_file_link).'" target="_blank" download>Download File</a></div></div>';
+    }else{
+        $login_page_link = home_url( '/registration/' );
+        return '<div class="pl-contact">To download this music for FREE to use in your vidoes:<div class="pl-cta"><a href="'.esc_url($login_page_link).'">Register for a FREE Account Here</a></div></div>';
+    }
+    return '';
+}
